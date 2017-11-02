b804b5187c0571cb2bf1513bd5de5bbe1c00f249
isan <isan@isan.isan>
2017-11-02 17:53:26 +0200

    first initial commit

---
+++ routes/bittrex.js
@@ -0,0 +1,86 @@
+var bittrex = require('node-bittrex-api');
+var express = require('express');
+var router = express.Router();
+
+// config
+bittrex.options({
+    API_KEY:'1008ea4bd3544382a7e5bebd12ecdd66',
+    API_SECRET:'4b912eb00861428289e0cdfeedb4f49e',
+});
+
+var ticker_returner = function(res,i,final,obj){
+    console.log("inside returner i:" + i + " final " + final);
+    if((i==final)){
+        res.json(obj);
+    }
+};
+//
+// router.get('/',function(req,res,next){
+// /*
+//     bittrex.getcandles({
+//         marketName: 'USDT-BTC',
+//         tickInterval: 'fiveMin', // intervals are keywords
+//     }, function( data, err ) {
+//         console.log( data );
+//         res.json(data);
+//     });
+// */
+//
+//     // bittrex.getmarketsummary( { market : 'BTC-LTC'}, function( data, err ) {
+//     //     console.log( data );
+//     // });
+//     //
+//     // bittrex.getmarkethistory({ market : 'BTC-LTC' }, function( data, err ) {
+//     //     console.log( data );
+//     // });
+//     //
+//     // bittrex.getorderbook({ market : 'BTC-LTC', depth : 10, type : 'both' }, function( data, err ) {
+//     //     console.log( JSON.stringify(data) );
+//     // });
+//     //
+//     // bittrex.getticker( { market : 'BTC-LTC' }, function( data, err ) {
+//     //     console.log( data );
+//     // });
+//     res.json({ronen:"ronen"});
+// });
+
+/* Subscribe to a market an get constant updates!!. */
+// router.get('/', function(req, res, next) {
+//     bittrex.websockets.subscribe(['USDT-ETH'], function(data, client) {
+//         if (data.M === 'updateExchangeState') {
+//             data.A.forEach(function(data_for) {
+//                 console.log('Market Update for '+ data_for.MarketName, data_for);
+//
+//             });
+//         }
+//     });
+//     res.json({waiting:"waiting"});
+//
+// });
+
+module.exports = router;
+//
+// function getAllMarketSummaries(callback){
+//     bittrex.getmarketsummaries( function( data, err ) {
+//         if (err) {
+//             return console.error(err);
+//         }
+//         callback(data);
+//         // data object
+//     });
+// }
+// function getAllTickers(){
+//     getAllMarketSummaries(function(data){
+//         for( var i in data.result ) {
+//             bittrex.getticker( { market : data.result[i].MarketName }, function( ticker ) {
+//                 console.log( ticker );
+//             })
+//     }
+// }
+// }
+
+/**
+ * Bittrex:
+ Key:1008ea4bd3544382a7e5bebd12ecdd66
+ Secret:4b912eb00861428289e0cdfeedb4f49e
+ */
