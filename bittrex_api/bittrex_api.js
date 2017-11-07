881d2758d511c597066428801193f596754e61c2
isan <isan@isan.isan>
2017-11-07 14:45:25 +0200

    buy/sell/order module completed

--- bittrex_api/bittrex_api.js
+++ bittrex_api/bittrex_api.js
@@ -1,20 +1,293 @@
 var bittrex = require('node-bittrex-api');
+var Client = require('node-rest-client').Client;
+var client = new Client();
+var jsonfile = require('jsonfile');
 
 var manager = {};
+var error_trigger = "";
+var coins_map = null;
 
-module.exports.setEventManager = function(event_manager){
+
+if (coins_map == null) {
+    var file = './coin_map.json';
+    jsonfile.readFile(file, function (err, obj) {
+        if(err)
+            console.log(err);
+        coins_map = obj;
+    });
+}
+
+module.exports.setEventManager = function(event_manager, error_trig){
     manager = event_manager;
+    error_trigger = error_trig;
 };
 
+/** order object
+ * {
+ * market: 'USDT-ETH'
+ * amount: '1.65'
+ * rate: 'BID/ASK/LAST'
+ * delay: seconds
+ * type: 'BUY/SELL'
+ * }
+ */
+
+/** getMarekeySummary : data object
+ *    { success: true,
+      message: '',
+      result:
+       [ { MarketName: 'BTC-LTC',
+           High: 0.008357,
+           Low: 0.00677804,
+           Volume: 165689.16074968,
+           Last: 0.007482,
+           BaseVolume: 1268.28735005,
+           TimeStamp: '2017-11-02T14:41:13.617',
+           Bid: 0.00745183,
+           Ask: 0.00748197,
+           OpenBuyOrders: 1263,
+           OpenSellOrders: 13780,
+           PrevDay: 0.00833811,
+           Created: '2014-02-13T00:00:00' } ] }
+ */
+
+/** Time Object
+ * {
+  BittrexTimeStamp:
+  OSTimeStamp:
+  Year:
+  Month:
+  Day:
+  Hour:
+  Minute:
+  Seconds:
+  Milliseconds:
+ * }
+ */
+
+/** coinMarketCap object:
+ *     {
+        "id": "omisego",
+        "name": "OmiseGO",
+        "symbol": "OMG",
+        "rank": "13",
+        "price_usd": "6.58227",
+        "price_btc": "0.00091541",
+        "24h_volume_usd": "21770900.0",
+        "market_cap_usd": "671671627.0",
+        "available_supply": "102042552.0",
+        "total_supply": "140245398.0",
+        "max_supply": null,
+        "percent_change_1h": "-0.81",
+        "percent_change_24h": "5.28",
+        "percent_change_7d": "-8.09",
+        "last_updated": "1510045764"
+    }
+ */
+
+/** order_receipt object:
+  {
+  id: TimeStamp$market$amount$type
+  received: {} V
+  status: true/false V
+  order: {} V
+  data: {} V
+  coinMarketCap: {} V
+  time: {}
+  }
+ */
+
+/**
+ * received object:(USDT-ETH => base = USDT , trade = ETH)
+ * {
+ * base:
+ * trade:
+ * rate:
+ * finalAmount:
+ * }
+ */
+
+
+/** Time Object
+ * {
+  BittrexTimeStamp:
+  OSTimeStamp:
+  Year:
+  Month_Name:
+  Month:
+  Day_Name:
+  Day:
+  Hour:
+  Minute:
+  Seconds:
+  Milliseconds:
+  OSDate:
+ * }
+ */
+// USDT-ETH
+// means: sell: ethereum
+function extractMarkets(market){
+    var market_obj ={};
+    var l = market.split("-");
+    market_obj.base = l[0];
+    market_obj.trade = l[1];
+    return market_obj;
+};
+
+/**
+ * received order related
+ */
+// construct recieved object
+function constructOrderReceived(order,market_data){
+    var received = {};
+    var coins = extractMarkets(order.market);
+    received.base = coins.base;
+    received.trade = coins.trade;
+    var rate = getRate(order,market_data);
+    received.rate = rate;
+    received.finalAmount = calcOrder(rate,order);
+    return received;
+}
+function calcOrder(rate,order){
+    var recieved;
+    if(order.type =="SELL")
+    {
+        recieved = rate * order.amount;
+    }
+    else if(order.type =="BUY"){
+        recieved = rate / order.amount;
+    }
+    return recieved;
+};
+function getRate(order,market_data){
+    var rate;
+
+    if(order.rate == "BID")
+    {
+        rate = market_data.result[0].Bid;
+    }
+    else if(order.rate == "ASK")
+    {
+        rate = market_data.result[0].Ask;
+    }
+    else if(order.rate == "LAST")
+    {
+        rate = market_data.result[0].Last;
+    }
+    return rate;
+}
+
+/**
+ * time object related
+ */
+
+function parseDate(str_date) {
+    return new Date(Date.parse(str_date));
+}
+
+function constructTime(bittrexTime)
+{
+    var time ={};
+    time.BittrexTime = bittrexTime;
+    time.OSTime =new Date();
+    var seperated = time.OSTime.toDateString().split(" ");
+    time.Day_Name = seperated[0];
+    time.Month_Name = seperated[1];
+    time.Month = time.OSTime.getMonth()+1;
+    time.Year = seperated[3];
+    time.Hours = time.OSTime.getHours();
+    time.Minute = time.OSTime.getMinutes();
+    time.Seconds = time.OSTime.getSeconds();
+    time.Milliseconds = time.OSTime.getMilliseconds();
+    var t = parseDate(time.OSTime).toTimeString();
+    var d= parseDate(time.OSTime).toDateString();
+    time.OSTime = t;
+    time.OSDate = d;
+    return time;
+}
+
+/**
+ * get state general
+ */
 // getState
-module.exports.getState = function(trigger_name,market){
-    bittrex.getmarketsummary( { market : market}, function( data, err ) {
+var getStateTest = function(trigger_name,order){
+    bittrex.getmarketsummary( { market : order.market}, function( data, err ) {
+        var state = {status:true};
         if(err)
         {
-            manager.trigger(trigger_name,err);
+            state.err = err;
+            manager.trigger(error_trigger,err);
         }
-        manager.trigger(trigger_name,data);
+        state.order = order;
+        state.data = data.result[0];
+        state.received = constructOrderReceived(order,data);
+        url = 'http://api.coinmarketcap.com/v1/ticker/'+coins_map[state.received.trade]+'/?convert=USD';
+        client.get(url, function (data_, response) {
+            state.coinMarketCap = data_[0];
+            state.time = constructTime(state.data.TimeStamp);
+            manager.trigger(trigger_name,state);
+        });
     });
 };
+function placeOrder(trigger_name,order) {
+    if (order.delay != null)
+    {
+        setTimeout(getState.bind(trigger_name,order), order.delay);
+    }
+    else
+    {
+        setTimeout(getState.bind(trigger_name,order), 0);
+    }
+};
 
+module.exports.order = function(trigger_name,order) {
+    placeOrder(trigger_name,order);
+};
+
+module.exports.buy = function(trigger_name,order) {
+    placeOrder(trigger_name,order);
+};
+
+module.exports.sell = function(trigger_name,order) {
+    placeOrder(trigger_name,order);
+};
+
+
+
+/**
+ *  TEST
+ */
+
+// getState
+// var getStateTest = function(trigger_name,order){
+//     bittrex.getmarketsummary( { market : order.market}, function( data, err ) {
+//         var state = {status:true};
+//         if(err)
+//         {
+//             state.err = err;
+//             console.log('error:' + err);
+//         }
+//         state.order = order;
+//         state.data = data.result[0];
+//         state.received = constructOrderReceived(order,data);
+//
+//         url = 'http://api.coinmarketcap.com/v1/ticker/'+coins_map[state.received.trade]+'/?convert=USD';
+//         client.get(url, function (data_, response) {
+//             state.coinMarketCap = data_[0];
+//             state.time = constructTime(state.data.TimeStamp);
+//             console.log(JSON.stringify(state,null,2));
+//         });
+//     });
+// };
+
+// test case
+//
+// var order = {
+//     market:'USDT-ETH',
+//     amount: 10,
+//     rate: "LAST",
+//     delay: 0,
+//     type: 'SELL'
+// };
+// getStateTest("",order);
 
